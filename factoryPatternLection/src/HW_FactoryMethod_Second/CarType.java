4cb586e256e18a10bcd1c3caf669f9a32891dd0c
Maratov Maksat <82010556+madmaaax@users.noreply.github.com>
2021-10-20 18:53:53 +0600

    Delete factoryPatternLection/src/HW_FactoryMethod_Second directory

--- factoryPatternLection/src/HW_FactoryMethod_Second/CarType.java
+++
@@ -1,7 +0,0 @@
-package HW_FactoryMethod_Second;
-
-public interface CarType {
-    String TESLA="Tesla";
-    String Toyota="Toyota Camry";
-    void info();
-}
--- factoryPatternLection/src/HW_FactoryMethod_Second/Test.java
+++
@@ -1,8 +0,0 @@
-package HW_FactoryMethod_Second;
-
-public class Test {
-    public static void main(String[] args) {
-        CarType car = SomeCarFactory.createCar(CarType.Toyota);
-        car.info();
-    }
-}
